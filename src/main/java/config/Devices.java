a845da8bfe408879ffd4223f2527f5a54c383723
Your Name <you@example.com>
2023-07-10 21:40:10 +0300

    - 10 07 2023

---
+++ src/main/java/config/Devices.java
@@ -0,0 +1,17 @@
+package config;
+
+public enum Devices {
+    PIXEL("PIXEL"),
+    PIXE4("Pixel4"),
+    PIXEL4Native("PixelNative");
+
+    public String getName() {
+        return name;
+    }
+
+    String name;
+
+    Devices(String name) {
+        this.name = name;
+    }
+}
